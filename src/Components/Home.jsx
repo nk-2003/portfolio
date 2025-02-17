d8661277ea61466d20aae574086d19cfa3f31d22
naveenkumar <iamnk1000@gmail.com>
2025-02-17 18:08:20 +0530

    Initial commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,7 +5,7 @@ import '../App.css';
 const Home = () => {
   return (
     <div className="home">
-      <img src="image.jpg" className="profile-image" alt="Profile" />
+      <img src="./images/image.jpg" className="profile-image" alt="Profile" />
       <h1>Welcome to My Portfolio</h1>
       <p className="intro-text">
         Hello there! I'm <strong>NAVEENKUMAR N</strong>, a passionate and creative web developer specializing in building stunning, responsive, and user-friendly websites. Dive into my projects and achievements, and let's connect to bring more digital dreams to life!
